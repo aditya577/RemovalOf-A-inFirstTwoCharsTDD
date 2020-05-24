d2980f648e264b461b3324fdbf3a4d9f0bbc945c
aditya <aditya@Warlord-pc>
2020-05-24 21:42:56 +0530

    made some more changes to satisfy second method, Result: GREEN

--- SecondTDD/src/main/StringHelper.java
+++ SecondTDD/src/main/StringHelper.java
@@ -4,12 +4,18 @@ public class StringHelper {
 
 	public String removeAatFirstTwoChars(String str) {
 		
+		String out = str;
+		
 		char first = str.charAt(0);
+		char second = str.charAt(1);
 		
 		if(first == 'A')
-			return str.substring(1);
+			out = str.substring(1);
 		
-		return str;
+		if(second == 'A')
+			out = out.substring(1);
+		
+		return out;
 	}
 
 }
