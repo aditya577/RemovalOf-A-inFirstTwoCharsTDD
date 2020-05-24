4ae8c46e58b44a5a1be3e1dd9e97947236d086ff
aditya <aditya@Warlord-pc>
2020-05-24 21:39:31 +0530

    making changes in main class to satisfy first test method, testIfAisPresentAtFirstChar(), Result: GREEN

--- SecondTDD/src/main/StringHelper.java
+++ SecondTDD/src/main/StringHelper.java
@@ -4,7 +4,10 @@ public class StringHelper {
 
 	public String removeAatFirstTwoChars(String str) {
 		
+		char first = str.charAt(0);
 		
+		if(first == 'A')
+			return str.substring(1);
 		
 		return str;
 	}
